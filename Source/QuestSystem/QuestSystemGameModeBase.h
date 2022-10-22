cd50f37a1f950b08a592acd0c052714ac21dfcb0
Josh <deadmonstor69@gmail.com>
2022-10-22 22:08:20 +0100

    Init commit

---
+++ Source/QuestSystem/QuestSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuestSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API AQuestSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
